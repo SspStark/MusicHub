461a18879d70a8ea1c5cc9e0651bcf68b059120b
SspStark <ssptonystark005@gmail.com>
2023-10-14 13:11:00 +0530

    Backend

--- Backend/sql.sql
+++ Backend/sql.sql
@@ -1,7 +1,7 @@
 --CREATE TABLE songs (song_name varchar(200), song_file BLOB);
 --insert into user (id,email,userame,password)
 --values ("1","ssp@gmail.com","stark","191916")
---select * from user
+select * from user
 --alter table user rename column userame to username 
 --drop table user
 
@@ -11,4 +11,4 @@
 
 --DROP TABLE songs
 
-SELECT * FROM songs
+--SELECT * FROM songs
