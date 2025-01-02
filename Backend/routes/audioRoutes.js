1078e852846631c0a7eedb43447d6e5ae12131e7
SspStark <ssptonystark005@gmail.com>
2025-01-02 13:43:38 +0530

    Renamed Frontend to client and Backend to server

--- Backend/routes/audioRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const audioController = require('../controllers/audioController');
-
-router.get('/audio', audioController.getAllAudios);
-router.post('/audio', audioController.addAudio);
-
-module.exports = router;
